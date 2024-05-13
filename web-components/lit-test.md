424569c7a065c104fdc1fe8963266e475f334c41
Matt Henry <matt.henry@gsa.gov>
2024-05-13 14:46:29 -0400

    don't use :host in scoped style

--- web-components/lit-test.md
+++ web-components/lit-test.md
@@ -3,3 +3,5 @@
 <usa-link href="http://designsystem.digital.gov">
 It's dangerous to go alone. Here, take this.
 </usa-link>
+
+[I'm a regular, non-`usa-link` link](https://18f.gsa.gov)
--- web-components/src/components/usa-link/index.js
+++ web-components/src/components/usa-link/index.js
@@ -27,29 +27,27 @@ export class UsaLink extends LitElement {
   }
 
   static styles = css`
-    :host {
-      a {
-        color: var(--theme-link-color, #005ea2);
-        text-decoration: underline;
-      }
-      a:visited {
-        color: var(--theme-link-visited-color, #54278f);
-      }
+    a {
+      color: var(--theme-link-color, #005ea2);
+      text-decoration: underline;
+    }
+    a:visited {
+      color: var(--theme-link-visited-color, #54278f);
+    }
 
-      a:hover {
-        color: var(--theme-link-hover-color, #1a4480);
-      }
+    a:hover {
+      color: var(--theme-link-hover-color, #1a4480);
+    }
 
-      a:active {
-        color: var(--theme-link-active-color, #162e51);
-      }
+    a:active {
+      color: var(--theme-link-active-color, #162e51);
+    }
 
-      a:focus {
-        /* @include focus-outline; */
-        outline: var(--theme-focus-width, 0.25rem)
-          var(--theme-focus-style, solid) var(--theme-focus-color, #2491ff);
-        outline-offset: var(--theme-focus-offset, 0);
-      }
+    a:focus {
+      /* @include focus-outline; */
+      outline: var(--theme-focus-width, 0.25rem) var(--theme-focus-style, solid)
+        var(--theme-focus-color, #2491ff);
+      outline-offset: var(--theme-focus-offset, 0);
     }
   `;
 }
