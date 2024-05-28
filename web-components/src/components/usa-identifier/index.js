3d348a91f425bec6c9ab4791fa570694b6d51083
amyleadem <amy.leadem@bixal.com>
2024-05-28 16:23:48 -0700

    Add scaffolding to elements

--- web-components/src/components/usa-identifier/index.js
+++ web-components/src/components/usa-identifier/index.js
@@ -1,130 +1,103 @@
 import { LitElement, html, unsafeCSS } from "lit";
-import { classMap } from "lit/directives/class-map.js";
-import {customElement, property} from 'lit/decorators.js';
-
+import uswdsCoreStyle from "@uswds/uswds/scss/uswds-core?inline";
 import usaIdentifierStyle from "@uswds/uswds/scss/usa-identifier?inline";
 
 export class UsaIdentifier extends LitElement {
-
-  static get properties() {
-    return {
-      /**
-       * The site domain
-       */
-      site_domain: { type: String },
-      /**
-       * The agency name
-       */
-      agency_name: { type: String },
-      /**
-       * The destination URL for the agency and logo link
-       */
-      agency_href: { type: String },
-      /**
-       * The destination URL for the agency and logo link
-       */
-      agency_logo: { type: String },
-      /**
-       * The required link list items
-       */
-      link_list: { type: Array },
-    };
-  }
-
   constructor() {
     super();
-    this.agency_href = "";
+    /**
+       * Scaffold the logo(s):
+       * Add necessary classes
+    */
+    const logoList = this.querySelectorAll('.usa-identifier__logo');
+    const logoImageList = this.querySelectorAll('.usa-identifier__logo img');
+    const logoImageListArr = Array.from(logoImageList);
+    logoImageListArr.forEach(logo => {
+      logo.classList.add("usa-identifier__logo-img");
+    });
+
+    /**
+       * Scaffold the required links list:
+       * Build an unordered list and add necessary attributes
+    */
+    const nav = this.querySelector('nav');
+    const linksArr = Array.from(nav.children);
+    const linksList = document.createElement("ul");
+
+    linksList.classList.add("usa-identifier__required-links-list");
+    nav.insertAdjacentElement("afterbegin", linksList);
+
+    linksArr.forEach(link => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("usa-identifier__required-links-item");
+      link.classList.add("usa-identifier__required-link");
+      listItem.appendChild(link);
+      linksList.appendChild(listItem);
+    });
+
+    /**
+      * Scaffold usagov text
+      * Add necessary attributes
+    */
+    const usagov = this.querySelector('.usa-identifier__usagov-description');
+    const usagovLink = this.querySelector('.usa-identifier__usagov-description a');
+    usagovLink.classList.add('usa-link');
+
+    /**
+      * Scaffold disclaimer text
+      * Wrap "An" in aria-hidden span
+    */
+    const disclaimer = this.querySelector('.usa-identifier__identity-disclaimer');
+    if ( disclaimer.textContent.includes("An") ){
+      disclaimer.innerHTML.replace('An', '<span aria-hidden="true" style="background:pink">An</span>');
+    };
+
+    // return elements
+    this.logo = logoList;
+    this.list = linksList;
+    this.usagov = usagov;
+    this.disclaimer = disclaimer;
   }
 
-  static styles = [unsafeCSS(usaIdentifierStyle)];
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  static styles = [
+    unsafeCSS(usaIdentifierStyle),
+    unsafeCSS(uswdsCoreStyle)
+  ];
 
   render() {
     return html`
-
       <div class="usa-identifier">
         <section
           class="usa-identifier__section usa-identifier__section--masthead"
-          aria-label="Agency identifier"
+          aria-label="Agency identifier,"
         >
           <div class="usa-identifier__container">
             <div class="usa-identifier__logos">
-              <a
-                class="usa-identifier__logo"
-                href="${this.agency_href}"
-                ><img
-                  class="usa-identifier__logo-img"
-                  src="${this.agency_logo}"
-                  alt="${this.agency_name} logo"
-                  role="img"
-              /></a>
+              ${this.logo}
             </div>
-            <section
-              class="usa-identifier__identity"
-              aria-label="Agency description,"
-            >
-              <p class="usa-identifier__identity-domain">${this.site_domain}</p>
-              <p class="usa-identifier__identity-disclaimer">
-                <span aria-hidden="true">An </span>official website of the
-                <a href="${this.agency_href}">${this.agency_name}</a>
-              </p>
+            <section class="usa-identifier__identity" aria-label="Agency description,">
+              <p class="usa-identifier__identity-domain"><slot name="domain"></slot></p>
+              <p class="usa-identifier__identity-usagov">${this.disclaimer}</p>
             </section>
           </div>
         </section>
         <nav
           class="usa-identifier__section usa-identifier__section--required-links"
-          aria-label="Important links,,,"
+          aria-label="Important links"
         >
           <div class="usa-identifier__container">
-            <ul class="usa-identifier__required-links-list">
-              <li class="usa-identifier__required-links-item">
-                <a
-                  href="javascript:void(0)"
-                  class="usa-identifier__required-link usa-link"
-                  >About &lt;Parent shortname&gt;</a
-                >
-              </li>
-              <li class="usa-identifier__required-links-item">
-                <a href="" class="usa-identifier__required-link usa-link"
-                  >Accessibility statement</a
-                >
-              </li>
-              <li class="usa-identifier__required-links-item">
-                <a href="" class="usa-identifier__required-link usa-link"
-                  >FOIA requests</a
-                >
-              </li>
-              <li class="usa-identifier__required-links-item">
-                <a href="" class="usa-identifier__required-link usa-link"
-                  >No FEAR Act data</a
-                >
-              </li>
-              <li class="usa-identifier__required-links-item">
-                <a href="" class="usa-identifier__required-link usa-link"
-                  >Office of the Inspector General</a
-                >
-              </li>
-              <li class="usa-identifier__required-links-item">
-                <a href="" class="usa-identifier__required-link usa-link"
-                  >Performance reports</a
-                >
-              </li>
-              <li class="usa-identifier__required-links-item">
-                <a href="" class="usa-identifier__required-link usa-link"
-                  >Privacy policy</a
-                >
-              </li>
-            </ul>
+            ${this.list}
           </div>
         </nav>
-        <section
-          class="usa-identifier__section usa-identifier__section--usagov"
-          aria-label="U.S. government information and services"
-        >
+        <section>
           <div class="usa-identifier__container">
             <div class="usa-identifier__usagov-description">
-              Looking for U.S. government information and services?
+              ${this.usagov}
             </div>
-            <a href="https://www.usa.gov/" class="usa-link">Visit USA.gov</a>
           </div>
         </section>
       </div>
