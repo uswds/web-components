46082c572011fd1dfa5bd8da96c31d14ac034592
amyleadem <amy.leadem@bixal.com>
2024-05-31 16:30:24 -0700

    Move language content to JSON

---
+++ web-components/src/components/usa-identifier-attributes-object/content/index.js
@@ -0,0 +1,2 @@
+export { default as DefaultContent } from "./usa-identifier.json";
+export { default as EsContent } from "./usa-identifier~lang-es.json";
--- web-components/src/components/usa-identifier-attributes-object/index.js
+++ web-components/src/components/usa-identifier-attributes-object/index.js
@@ -1,6 +1,7 @@
 import { LitElement, html, unsafeCSS } from "lit";
 import uswdsCoreStyle from "@uswds/uswds/scss/uswds-core?inline";
 import usaIdentifierStyle from "@uswds/uswds/scss/usa-identifier?inline";
+import { DefaultContent, EsContent } from "./content";
 
 export class UsaIdentifierAttributesObject extends LitElement {
   static get properties() {
@@ -30,51 +31,19 @@ export class UsaIdentifierAttributesObject extends LitElement {
   ];
 
   render() {
-    // English content
-    const linksEn = {
-      "about": "About",
-      "accessibility": "Accessibility statement",
-      "FOIA": "FOIA requests",
-      "noFEAR": "No FEAR Act data",
-      "OIG": "Office of the Inspector General",
-      "performance": "Performance reports",
-      "privacy": "Privacy policy"
-    };
-    const usagovEn = {
-      "introText" : "Looking for U.S. government information and services?",
-      "linkText": "Visit USA.gov",
-      "linkHref": "https://www.usa.gov/"
-    };
+    const linkItemClasses = "usa-identifier__required-links-item";
+    const linkClasses="usa-identifier__required-link usa-link";
 
-    // Spanish content
-    const linksEs = {
-      "about": "Acerca de",
-      "accessibility": "Declaración de accesibilidad",
-      "FOIA": "Solicitud a través de FOIA",
-      "noFEAR": "Datos de la ley No FEAR",
-      "OIG": "Oficina del Inspector General",
-      "performance": "Informes de desempeño",
-      "privacy": "Política de privacidad"
-    };
-    const usagovEs = {
-      "introText": "¿Necesita información y servicios del Gobierno?",
-      "linkText": "Visite USA.gov en Español",
-      "linkHref": "https://www.usa.gov/espanol/"
-    }
-
-    let linkNames = linksEn;
-    let usagovText = usagovEn;
-
+    let content = DefaultContent;
     if (this.language ==="es") {
-      linkNames = linksEs;
-      usagovText = usagovEs;
+      content = EsContent;
     };
 
     return html`
       <div class="usa-identifier">
         <section
           class="usa-identifier__section usa-identifier__section--masthead"
-          aria-label="Agency identifier"
+          aria-label="${content.aria_label}"
         >
           <div class="usa-identifier__container">
             <div class="usa-identifier__logos">
@@ -106,53 +75,53 @@ export class UsaIdentifierAttributesObject extends LitElement {
         >
           <div class="usa-identifier__container">
             <ul class="usa-identifier__required-links-list">
-              <li class="usa-identifier__required-links-item">
+              <li class="${linkItemClasses}">
                 <a
                   href="${this.links.about}"
-                  class="usa-identifier__required-link usa-link"
-                  >${linkNames.about} ${this.parentAgency.shortname}</a
+                  class="${linkClasses}"
+                  >${content.required_links.about} ${this.parentAgency.shortname}</a
                 >
               </li>
-              <li class="usa-identifier__required-links-item">
+              <li class="${linkItemClasses}">
                 <a
                   href="${this.links.accessibility}"
-                  class="usa-identifier__required-link usa-link"
-                  >${linkNames.accessibility}</a
+                  class="${linkClasses}"
+                  >${content.required_links.accessibility}</a
                 >
               </li>
-              <li class="usa-identifier__required-links-item">
+              <li class="${linkItemClasses}">
                 <a
                   href="${this.links.FOIA}"
-                  class="usa-identifier__required-link usa-link"
-                  >${linkNames.FOIA}</a
+                  class="${linkClasses}"
+                  >${content.required_links.FOIA}</a
                 >
               </li>
-              <li class="usa-identifier__required-links-item">
+              <li class="${linkItemClasses}">
                 <a
                   href="${this.links.NoFEAR}"
-                  class="usa-identifier__required-link usa-link"
-                  >${linkNames.noFEAR}</a
+                  class="${linkClasses}"
+                  >${content.required_links.noFEAR}</a
                 >
               </li>
-              <li class="usa-identifier__required-links-item">
+              <li class="${linkItemClasses}">
                 <a
                   href="${this.links.OIG}"
-                  class="usa-identifier__required-link usa-link"
-                  >${linkNames.OIG}</a
+                  class="${linkClasses}"
+                  >${content.required_links.OIG}</a
                 >
               </li>
-              <li class="usa-identifier__required-links-item">
+              <li class="${linkItemClasses}">
                 <a
                   href="${this.links.performance}"
-                  class="usa-identifier__required-link usa-link"
-                  >${linkNames.performance}</a
+                  class="${linkClasses}"
+                  >${content.required_links.performance}</a
                 >
               </li>
-              <li class="usa-identifier__required-links-item">
+              <li class="${linkItemClasses}">
                 <a
                   href="${this.links.privacy}"
-                  class="usa-identifier__required-link usa-link"
-                  >${linkNames.privacy}</a
+                  class="${linkClasses}"
+                  >${content.required_links.privacy}</a
                 >
               </li>
             </ul>
@@ -160,13 +129,13 @@ export class UsaIdentifierAttributesObject extends LitElement {
         </nav>
         <section
           class="usa-identifier__section usa-identifier__section--usagov"
-          aria-label="U.S. government information and services"
+          aria-label="${content.usagov.aria_label}"
         >
           <div class="usa-identifier__container">
             <div class="usa-identifier__usagov-description">
-              ${usagovText.introText}
+              ${content.usagov.description}
             </div>
-            <a href="${usagovText.linkHref}" class="usa-link">${usagovText.linkText}</a>
+            <a href="${content.usagov.link.url}" class="usa-link">${content.usagov.link.label}</a>
           </div>
         </section>
       </div>
