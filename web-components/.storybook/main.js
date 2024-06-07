b7e6b9dbc9a9df737b8c2768b733b2f402a7eb84
James Mejia <james.mejia@gsa.gov>
2024-06-07 15:40:16 -0500

    Update story paths.

--- web-components/.storybook/main.js
+++ web-components/.storybook/main.js
@@ -1,13 +1,8 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
   stories: [
-    "../stories/**/*.mdx",
     "../src/**/*.mdx",
     "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    // TODO: Delete once we know whether we'll add stories in this directory.
-    // Storybook recommends stories living alongside components.
-    // See: https://storybook.js.org/docs/writing-stories#where-to-put-stories
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
