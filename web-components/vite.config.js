87219b363f04f35b69ea306be8bd8490cef2b450
James Mejia <james.mejia@gsa.gov>
2024-06-07 15:45:03 -0500

    Include Public Sans to vite config for Storybook.

--- web-components/vite.config.js
+++ web-components/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  assetsInclude: ['./node_modules/@uswds/uswds/fonts/public-sans'],
   test: {
     globals: true,
     environment: "jsdom",
