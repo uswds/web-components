0b77fcef9bab5a5f49ef4194769fbaa2906ac7c4
James Mejia <james.mejia@gsa.gov>
2024-06-10 13:24:32 -0500

    Remove fonts from assetsInclude.
    Added to load fonts locally, but seems like its intended for including assets in final build.

--- web-components/vite.config.js
+++ web-components/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  assetsInclude: ['./node_modules/@uswds/uswds/fonts/public-sans'],
   test: {
     globals: true,
     environment: "jsdom",
