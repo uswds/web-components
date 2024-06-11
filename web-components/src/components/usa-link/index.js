f367870120115e5cb1dd3f610327b364e798ccc6
Matt Henry <matt.henry@gsa.gov>
2024-06-11 15:33:01 -0400

    Add jsdoc comment

--- web-components/src/components/usa-link/index.js
+++ web-components/src/components/usa-link/index.js
@@ -2,10 +2,18 @@ import { LitElement, css, html } from "lit";
 import styles from "./usa-link.css.js";
 
 /**
- * An example element.
+ * @summary The usa-link component.
  *
  * @slot - This element has a slot
- * @csspart
+ *
+ * @attribute {String} href - The url for the link
+ *
+ * @cssprop --theme-link-color - Sets the link color
+ * @cssprop --theme-link-visited-color - Sets the color for visited links
+ * @cssprop --theme-link-hover-color - Sets the hover state link color
+ * @cssprop --theme-link-active-color - Sets the active state link color
+ *
+ * @tagname usa-link
  */
 export class UsaLink extends LitElement {
   static styles = [styles];
