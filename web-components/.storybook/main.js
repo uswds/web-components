ad19ad2a8eea560e137fb12c98edf86cc0d4fe8f
amyleadem <amy.leadem@bixal.com>
2024-06-11 14:26:55 -0700

    Merge branch 'jm/add-storybook'

---
+++ web-components/.storybook/main.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+  ],
+  framework: {
+    name: "@storybook/web-components-vite",
+    options: {},
+  },
+};
+export default config;
---
+++ web-components/.storybook/preview.js
@@ -0,0 +1,26 @@
+import UswdsTheme from "./UswdsTheme";
+
+// Global component styles.
+import "../src/index.css";
+// Theme overrides.
+import "./index.css";
+
+
+/** @type { import('@storybook/web-components').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      toc: true, // Autogenerate table of contents.
+      theme: UswdsTheme,
+    }
+  },
+  tags: ['autodocs'],
+};
+
+export default preview;
