06473f9f8f21951fedbfa546c1aa5068f3b5f981
amyleadem <amy.leadem@bixal.com>
2024-06-13 16:37:46 -0700

    Move data out of JSON into args

--- web-components/src/components/usa-identifier/content/index.js
+++
@@ -1,6 +0,0 @@
-export { default as DefaultContent } from "./usa-identifier.json";
-export { default as EsContent } from "./usa-identifier~lang-es.json";
-export { default as SecondaryLogoContent } from "./usa-identifier~secondary-logos.json";
-export { default as EsSecondaryLogoContent } from "./usa-identifier~secondary-logo-lang-es.json";
-export { default as TaxpayerContent } from "./usa-identifier~taxpayer-disclaimer.json";
-export { default as EsTaxpayerContent } from "./usa-identifier~taxpayer-disclaimer-lang-es.json";
--- web-components/src/components/usa-identifier/identifier.stories.js
+++ web-components/src/components/usa-identifier/identifier.stories.js
@@ -1,93 +1,128 @@
 import "./index";
 import { html } from 'lit';
-import {
-  DefaultContent,
-  EsContent,
-  SecondaryLogoContent,
-  EsSecondaryLogoContent,
-  TaxpayerContent,
-  EsTaxpayerContent,
-} from "./content";
 
 export default {
   title: "Components/Identifier",
   component: "usa-identifier",
+  argTypes: {
+    primary_agency: { name: "Primary agency information" },
+    logo1: { control: 'text', name: "Primary agency logo" },
+    secondary_agency: {name: "Secondary agency information" },
+    logo2: { control: 'text', name: "Secondary agency logo" },
+    masthead: { name: "Masthead content" },
+    taxpayer: { name: "Taxpayer disclaimer" },
+    required_links: { name: "Required links" },
+    usagov: { name: "USA.gov information" }
+  },
+  args: {
+    primary_agency: {
+      name: "[Parent agency]",
+      shortname: "[Agency shortname]",
+      url: "javascipt:void(0)",
+    },
+    logo1: "https://designsystem.digital.gov/assets/img/circle-gray-20.svg",
+    secondary_agency: {
+      name: "[Other agency]",
+      url: "javascipt:void(0)",
+    },
+    logo2: "https://designsystem.digital.gov/assets/img/circle-gray-20.svg",
+    masthead: {
+      conjunction: "and",
+      domain: "[domain.gov]",
+      disclaimer: "An official website of the",
+    },
+    taxpayer: "Produced and published at taxpayer expense.",
+    required_links: {
+      about: "About",
+      accessibility: "Accessibility statement",
+      foia: "FOIA requests",
+      no_FEAR: "No FEAR Act data",
+      oig: "Office of the Inspector General",
+      performance: "Performance reports",
+      privacy: "Privacy policy",
+    },
+    usagov: {
+      text: "Looking for U.S. government information and services?",
+      link_label: "Visit USA.gov",
+      link_url: "https://www.usa.gov/"
+    }
+  },
   render: ({
+    primary_agency,
+    secondary_agency,
+    logo1,
+    logo2,
     masthead,
+    taxpayer,
     required_links,
-    usagov,
-    parent_logo,
-    parent_secondary,
-    taxpayer_disclaimer
+    usagov
   }) => html`
     <usa-identifier>
-      ${masthead.domain ? html`<p slot="domain">${masthead.domain}</p>`: null}
-      ${parent_logo ? html`
-        <a slot="logo" href="${masthead.parent.url}">
-          <img
-            src="${parent_logo}"
-            alt="${masthead.parent.name} logo"/>
-        </a>
-      `: null}
-      ${parent_secondary ? html`
-        <a slot="logo" href="${parent_secondary.url}">
-          <img
-            src="${parent_secondary.logo}"
-            alt="${parent_secondary.name} logo"/>
-        </a>
-      `: null}
+      <p slot="domain">${masthead.domain}</p>
+      ${logo1 ? html`<a slot="logo" href="${primary_agency.url}">
+        <img src="${logo1}" alt="${primary_agency.name} logo"/>
+      </a>`: null}
+      ${logo2 ? html`<a slot="logo" href="${secondary_agency.url}">
+        <img src="${logo2}" alt="${secondary_agency.name} logo"/>
+      </a>`: null}
       <p slot="disclaimer">
-        ${masthead.content}
-        <a href="">${masthead.parent.name}</a>
-        ${parent_secondary ? html`${parent_secondary.conjunction} <a href="">${parent_secondary.name}</a>`: null}
-        ${taxpayer_disclaimer ? html`. ${taxpayer_disclaimer}`: null}
+        ${masthead.disclaimer} <a href="${primary_agency.url}">${primary_agency.name}</a>
+        ${secondary_agency ? html`${masthead.conjunction} <a href="${secondary_agency.url}">${secondary_agency.name}</a>`: null}${taxpayer ? html`. ${taxpayer}`: null}
       </p>
       <nav slot="links">
-        <a href="${required_links.about.url}">${required_links.about.label} ${parent.shortname}</a>
-        <a href="${required_links.accessibility.url}">${required_links.accessibility.label}</a>
-        <a href="${required_links.foia.url}">${required_links.foia.label}</a>
-        <a href="${required_links.no_FEAR.url}">${required_links.no_FEAR.label}</a>
-        <a href="${required_links.oig.url}">${required_links.oig.label}</a>
-        <a href="${required_links.performance.url}">${required_links.performance.label}</a>
-        <a href="${required_links.privacy.url}">${required_links.privacy.label}</a>
+        <a href="">${required_links.about} ${primary_agency.shortname}</a>
+        <a href="">${required_links.accessibility}</a>
+        <a href="">${required_links.foia}</a>
+        <a href="">${required_links.no_FEAR}</a>
+        <a href="">${required_links.oig}</a>
+        <a href="">${required_links.performance}</a>
+        <a href="">${required_links.privacy}</a>
       </nav>
-      <div slot="usagov">${usagov.description} <a href="${usagov.link.url}">${usagov.link.label}</a></div>
+      <div slot="usagov">
+        ${usagov.text} <a href="${usagov.link_url}">${usagov.link_label}</a>
+      </div>
     </usa-identifier>
   `
 }
 
 export const Default = {};
-Default.args = DefaultContent;
 
-export const DefaultSpanish = {};
-DefaultSpanish.args = EsContent;
-
-export const MultipleLogos = {};
-MultipleLogos.args = {
-  ...Default.args,
-  ...SecondaryLogoContent
+export const oneAgency = {
+  args: {
+    logo2: false,
+    secondary_agency: false,
+    taxpayer: false
+  },
+  argTypes: {
+    logo2: { table: { disable: true } },
+    secondary_agency: { table: { disable: true } },
+    taxpayer: { table: { disable: true } },
+  }
 };
 
-export const MultipleLogosSpanish = {};
-MultipleLogosSpanish.args = {
-  ...DefaultSpanish.args,
-  ...EsSecondaryLogoContent
+export const MultipleAgencies = {
+  args: {
+    taxpayer: false
+  },
+  argTypes: {
+    taxpayer: { table: { disable: true } },
+  }
 };
 
-export const NoLogo = {};
-NoLogo.args = {
-  ...DefaultContent,
-  parent_logo: null
-}
+export const NoLogo = {
+  args: {
+    logo1: false,
+    logo2: false,
+    secondary_agency: false,
+    taxpayer: false,
+  },
+  argTypes: {
+    logo1: { table: { disable: true } },
+    logo2: { table: { disable: true } },
+    secondary_agency: { table: { disable: true } },
+    taxpayer: { table: { disable: true } },
+  }
+};
 
 export const TaxpayerDisclaimer = {};
-TaxpayerDisclaimer.args = {
-  ...DefaultContent,
-  ...TaxpayerContent
-}
 
-export const TaxpayerDisclaimerSpanish = {};
-TaxpayerDisclaimerSpanish.args = {
-  ...DefaultContent,
-  ...EsTaxpayerContent
-}
