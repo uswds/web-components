e25ca9775891df4f976a25b506780116fa74457c
amyleadem <amy.leadem@bixal.com>
2024-06-21 07:39:41 -0700

    Add css preprocessor to vite config

--- web-components/vite.config.js
+++ web-components/vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ["./node_modules/@uswds/uswds/packages"],
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
