671abca7571eaa63e84c7910fa3746c589043209
amyleadem <amy.leadem@bixal.com>
2024-07-01 15:33:31 -0700

    Add accordion styles in JS; Add multiple details items

--- web-components/src/components/usa-details/details.stories.js
+++ web-components/src/components/usa-details/details.stories.js
@@ -6,28 +6,64 @@ export default {
   title: "Components/Details",
   component: "usa-details",
   argTypes: {
-    detailsSummary: {name: "Summary content"},
-    detailsContent: { name: "Panel content"},
-    open: { name: "Open panel on load" },
-    name: { name: "Details group name" },
+    item1Summary: {name: "Item 1 - Summary text"},
+    item1Content: { name: "Item 1 - Panel content"},
+    item1Open: { name: "Item 1 - Open panel on load"},
+    item2Show: { name: "Include item 2 in preview"},
+    item2Summary: {name: "Item 2 - Summary text", if: { arg: 'item2Show' } },
+    item2Content: { name: "Item 2 - Panel content", if: { arg: 'item2Show' } },
+    item2Open: { name: "Item 2 - Open panel on load", if: { arg: 'item2Show' } },
+    item3Show: { name: "Include item 3 in preview"},
+    item3Summary: {name: "Item 3 - Summary text", if: { arg: 'item3Show' } },
+    item3Content: { name: "Item 3 - Panel content", if: { arg: 'item3Show' } },
+    item3Open: { name: "Item 3 - Open panel on load", if: { arg: 'item3Show' } },
+    groupName: { name: "Details group name" },
   },
   args: {
-    detailsSummary: "Great Smoky Mountains National Park",
-    detailsContent: "The sprawling landscape encompasses lush forests and an abundance of wildflowers that bloom year-round. ",
-    name: "",
-    open: false,
+    item1Summary: "First Amendment",
+    item1Content: "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.",
+    item1Open: false,
+    item2Show: false,
+    item2Summary: "Second Amendment",
+    item2Content: "A well regulated Militia, being necessary to the security of a free State, the right of the people to keep and bear Arms, shall not be infringed.",
+    item2Open: false,
+    item3Show: false,
+    item3Summary: "Third Amendment",
+    item3Content: "No Soldier shall, in time of peace be quartered in any house, without the consent of the Owner, nor in time of war, but in a manner to be prescribed by law.",
+    item3Open: false,
+    groupName: "",
   },
   render: ({
-    detailsSummary,
-    detailsContent,
-    name,
-    open
+    item1Summary,
+    item1Content,
+    item1Open,
+    item2Show,
+    item2Summary,
+    item2Content,
+    item2Open,
+    item3Show,
+    item3Summary,
+    item3Content,
+    item3Open,
+    groupName,
   }) => html`
-    <usa-details open=${open || nothing} name=${name || nothing}>
-      <details>
-        <summary>${detailsSummary}</summary>
-        <div slot="details-content">${detailsContent}</div>
+    <usa-details>
+      <details open=${item1Open || nothing} name=${groupName || nothing}>
+        <summary>${item1Summary}</summary>
+        <div slot="details__body">${item1Content}</div>
       </details>
+      ${item2Show ? html`
+        <details open=${item2Open || nothing} name=${groupName || nothing}>
+          <summary>${item2Summary}</summary>
+          <div slot="details__body">${item2Content}</div>
+        </details>
+      `: null}
+      ${item3Show ? html`
+        <details open=${item3Open || nothing} name=${groupName || nothing}>
+          <summary>${item3Summary}</summary>
+          <div slot="details__body">${item3Content}</div>
+        </details>
+      `: null}
     </usa-details>
   `,
 };
@@ -36,12 +72,23 @@ export const Default = {};
 
 export const Open = {
   args: {
-    open: true,
+    item1Open: true,
   },
 }
 
-export const GroupName = {
+export const Group = {
   args: {
-    name: "example-group-name",
+    groupName: "example-group-name",
+    item1Open: true,
+    item2Show: true,
+    item3Show: true,
+  },
+}
+
+export const MultiselectGroup = {
+  args: {
+    item1Open: true,
+    item2Show: true,
+    item3Show: true,
   },
 }
--- web-components/src/components/usa-details/index.js
+++ web-components/src/components/usa-details/index.js
@@ -7,8 +7,8 @@ import usaAccordionStyle from "@uswds/uswds/scss/usa-accordion?inline";
  *
  * @slot - This element has a slot
  *
- * @attribute {Boolean} detailsOpen - Set the panel to be open on load
- * @attribute {String} detailsName - Add a group name if the element is part of a details group
+ * @attribute {Boolean} open - Set the panel to be open on load
+ * @attribute {String} name - Add a group name if the element is part of a details group
  *
  * @tagname usa-details
  */
@@ -16,6 +16,64 @@ export class UsaDetails extends LitElement {
   static styles = [
     unsafeCSS(usaAccordionStyle),
     unsafeCSS(uswdsCoreStyle),
+    css`
+      .details {
+        font-family: Source Sans Pro Web,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
+        list-style-type: none;
+        color: #1b1b1b;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        font-size: 1.06rem;
+        line-height: 1.5;
+      }
+      .summary {
+        font-family: Source Sans Pro Web,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
+        font-size: 1.06rem;
+        line-height: .9;
+        margin: 0;
+        margin-top: .5rem;
+        background-image: url(https://designsystem.digital.gov/assets/img/usa-icons/add.svg),linear-gradient(transparent,transparent);
+        background-repeat: no-repeat;
+        background-size: 1.5rem;
+        border: 0;
+        border-radius: 0;
+        box-shadow: none;
+        justify-content: normal;
+        text-align: left;
+        padding: 0;
+        color: #1b1b1b;
+        background-color: #f0f0f0;
+        background-repeat: no-repeat;
+        background-position: right 1.25rem center;
+        background-size: 1.5rem;
+        cursor: pointer;
+        font-weight: 700;
+        margin: 0;
+        padding: 1rem 3.5rem 1rem 1.25rem;
+        max-width: 100%;
+      }
+      .content {
+        overflow: visible;
+        padding-bottom: 1.5rem;
+        padding-left: 1rem;
+        padding-top: 1.5rem;
+        color: #1b1b1b;
+        background-color: #fff;
+        margin-top: 0;
+        overflow: auto;
+        padding: 1rem 1.25rem calc(1rem - .25rem);
+      }
+      .details[open] .summary {
+        background-image: url(https://designsystem.digital.gov/assets/img/usa-icons/remove.svg),linear-gradient(transparent,transparent);
+      }
+      details > summary {
+        list-style: none;
+      }
+      details > summary::-webkit-details-marker {
+        display: none;
+      }
+    `
   ];
 
   static properties = {
@@ -25,21 +83,27 @@ export class UsaDetails extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    this.details = this.querySelector("details");
-    this.open = this.details.getAttribute("open") || this.getAttribute("open");
-    this.name = this.details.getAttribute("name") || this.getAttribute("name");
-    this.summary = this.querySelector("[slot='details-summary']") || this.querySelector("summary");
-    this.content = this.querySelector("[slot='details-content']");
+    this.details = [...this.querySelectorAll('details')];
   }
 
   template() {
-    console.log(this.name)
     return html`
-      <details open="${this.open || nothing}" name="${this.name || nothing}">
-        ${this.summary}
-        ${this.content}
-      </details>
-    `
+      ${this.details.map((detail) => {
+          this.summary = detail.querySelector('summary');
+          this.content = detail.querySelector('[slot="details__body"]');
+          this.open = detail.getAttribute('open');
+          this.name = detail.getAttribute('name');
+          this.summary.classList.add('summary');
+          this.content.classList.add('content');
+          return html`
+            <details class="details" open="${this.open || nothing}" name="${this.name || nothing}">
+              ${this.summary}
+              ${this.content}
+            </details>
+          `
+          ;
+      })}
+    `;
   }
 
   render() {
