aea6026bb3a2923715f098fef3fb1e5ce009fe82
amyleadem <amy.leadem@bixal.com>
2024-07-16 17:06:02 -0700

    Add css custom props; format css

--- src/components/usa-identifier/usa-identifier.css.js
+++ src/components/usa-identifier/usa-identifier.css.js
@@ -1,42 +1,49 @@
 import { css } from "lit";
+import "../uswds-core/system-vars.css";
+import "../uswds-core/theme-vars.css";
 
 export default css`
   :host {
+    --usa-theme-identifier-background-color: var(--usa-system-color-ink);
+    --usa-theme-identifier-font-family: var(--usa-theme-font-body);
+    --usa-theme-identifier-max-width: var(--usa-theme-grid-container-max-width);
+    --usa-theme-identifier-primary-text-color: var(--usa-system-color-gray-10);
+    --usa-theme-identifier-secondary-text-color: var(--usa-system-color-gray-cool-30);
+
     .usa-identifier {
-      font-family: Public Sans Web,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
-      font-size: 1rem;
-      line-height: 1.4;
-      color: #fff;
-      background-color: #1b1b1b;
-      padding-bottom: 1rem;
+      background-color: var(--usa-theme-identifier-background-color);
+      font-family: var(--usa-theme-identifier-font-family);
+      font-size: 1.06rem;
+      line-height: 1.3;
+      padding-bottom: var(--usa-system-unit-2);
+      writing-mode: horizontal-tb;
     }
 
     .usa-identifier__section {
-      padding-bottom: 1rem;
-      padding-top: 1rem;
+      padding-block: var(--usa-system-unit-2);
     }
 
     .usa-identifier__container {
       margin-left: auto;
       margin-right: auto;
-      padding-left: 1rem;
-      padding-right: 1rem;
-      max-width: 87.5rem;
+      max-width: var(--usa-theme-identifier-max-width);
+      padding-left: var(--usa-system-unit-2);
+      padding-right: var(--usa-system-unit-2);
     }
 
     .usa-identifier__logos {
       display: flex;
-      margin-right: 1rem;
+      margin-right: var(--usa-system-unit-2);
     }
 
     .usa-identifier__logo {
-      text-decoration: none;
-      height: 3rem;
       display: block;
+      height: var(--usa-system-unit-6);
+      text-decoration: none;
     }
 
     .usa-identifier__logo+.usa-identifier__logo {
-      margin-left: .5rem;
+      margin-left: var(--usa-system-unit-1);
     }
 
     .usa-identifier__logo-img {
@@ -46,11 +53,11 @@ export default css`
 
     .usa-identifier__identity {
       flex: 1 1 0%;
-      margin-top: 1rem;
+      margin-top: var(--usa-system-unit-2);
     }
 
     .usa-identifier__identity-domain {
-      color: #adadad;
+      color: var(--usa-theme-identifier-secondary-text-color);
     }
 
     .usa-identifier__identity-disclaimer,
@@ -61,37 +68,41 @@ export default css`
 
     .usa-identifier__required-links-list {
       list-style-type: none;
-      padding-left: 0;
-      margin-bottom: 0;
-      margin-top: 0;
+      margin: 0;
+      padding: 0;
     }
 
     .usa-identifier__required-links-item {
       -moz-column-break-inside: avoid;
       break-inside: avoid;
-      margin-bottom: .75rem;
+      margin-bottom: var(--usa-system-unit-105);
     }
 
     .usa-identifier__identity-disclaimer {
-      font-weight: 700;
+      font-weight: var(--usa-theme-text-bold);
     }
 
     .usa-identifier__identity-disclaimer a,
     .usa-identifier__identity-disclaimer a:visited,
     .usa-identifier__usagov-description a,
     .usa-identifier__usagov-description a:visited {
-      color: #e6e6e6;
+      color: var(--usa-theme-identifier-primary-text-color);
     }
 
-    .usa-identifier__required-link, .usa-identifier__required-link.usa-link {
-      color: #adadad;
+    .usa-identifier__required-link,
+    .usa-identifier__required-link.usa-link {
+      color: var(--usa-theme-identifier-secondary-text-color);
       display: inline-block;
+    }
+
+    .usa-identifier__required-links-item:last-child {
+      margin-bottom: 0;
     }
 
     .usa-identifier__section--usagov a {
-      color: #e6e6e6;
-      font-weight: 700;
+      color: var(--usa-theme-identifier-primary-text-color);
       display: inline-block;
+      font-weight: var(--usa-theme-text-bold);
     }
 
     @media (min-width: 40em) {
@@ -111,8 +122,7 @@ export default css`
         column-fill: balance;
       }
       .usa-identifier__section--required-links {
-        padding-bottom: .5rem;
-        padding-top: .5rem;
+        padding-block: var(--usa-system-unit-1);
       }
     }
 
@@ -125,11 +135,10 @@ export default css`
         font-size: 1rem;
       }
       .usa-identifier__required-links-item {
-        margin-bottom: .5rem;
+        margin-bottom: var(--usa-system-unit-1);
       }
       .usa-identifier__container {
-        padding-left: 2rem;
-        padding-right: 2rem;
+        padding-inline: var(--usa-system-unit-4);
       }
     }
   }
