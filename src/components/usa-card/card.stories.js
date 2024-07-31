37f2ed328d92064b0ec5409ff6e5d31e80f0a3dd
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2024-07-31 13:32:34 -0400

    refactor for slot approach and add test story

--- src/components/usa-card/card.stories.js
+++ src/components/usa-card/card.stories.js
@@ -1,7 +1,7 @@
 import "./index";
 import "../usa-card-group/index";
 
-import { html, nothing} from "lit";
+import { html, nothing } from "lit";
 
 export default {
   title: "Components/Card",
@@ -231,6 +231,44 @@ export const ExdentMedia = {
   }
 }
 
+export const FullExdent = {
+  argTypes: {
+    headerFirst: {
+      table: {
+        disable: true
+      }
+    },
+    layout: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Extends the media element out over the card border.'
+      }
+    }
+  },
+  render: ({ title, media, content, buttonText }) => {
+    return html`
+      <usa-card exdent>
+        <img slot="card-media" src="${media}" alt="Placeholder image">
+        <div slot="card-header">
+          <h2 part="card-heading">${title || "Exdent media"}</h2>
+        </div>
+        <p>
+          ${content}
+        </p>
+        <div slot="card-footer">
+          <a href="#" class="usa-button">${buttonText}</a>
+        </div>
+      </usa-card>
+    `
+  }
+}
+
 export const Flag = {
   args: {
     layout: "flag",
@@ -245,9 +283,9 @@ export const Flag = {
   render: ({ title, media, content, buttonText, headerFirst, layout }) => {
     return html`
       <usa-card ?header-first=${headerFirst} layout="${layout == 'default' ? nothing : layout}">
-        <img slot="card-media" src="${media}" alt="Placeholder image">
+        <img slot="card-media" src="${media}" alt="Placeholder image"  class="usa-card__image">
         <div slot="card-header">
-          <h2 part="card-heading">${title || "Default flag"}</h2>
+          <h2 class="card-heading">${title || "Default flag"}</h2>
         </div>
         <p>
           ${content}
@@ -326,7 +364,7 @@ export const CardGroup = {
           </div>
           <p>
             ${content}
-          </p>>
+          </p>
           <div slot="card-footer">
             <a href="#" class="usa-button">${buttonText}</a>
           </div>
@@ -403,24 +441,57 @@ export const CardGroup = {
 }
 
   export const TestStory = {
-    render: () => {
+    render: ({ media, title, content, buttonText }) => {
       return html`
         <usa-card-group>
           <usa-card>
-            <div slot="card-header">
-              <h1 part="card-heading" >Header 1</h1>
+            <div slot="card-header" part="card-header">
+              <h1 part="cardheading">Header 1</h1>
+              <h2>Subheading</h2>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam neque ullam quia harum quaerat debitis, et minima fugit expedita hic?
+              This example uses a div with the slot "card-header" to put both headings in the header section. We are unable to add styles to the nested heading elements with the custom element stylesheet because they are children of the slotted element.
             </p>
-            <p>I am a p tag outside of the body slot! Captured via the generic slot and added to they card body template.</p>
             <div slot="card-footer">
-              <a href="#" class="test-button">
+              <a href="#" class="usa-button">
                 click me
               </a>
             </div>
           </usa-card>
           <usa-card>
+            <h1 part="cardheading" slot="card-header" class="usa-card__heading">Header 1</h1>
+            <h2 slot="card-header">Subheading</h2>
+            <p>
+              This example does not use a div wrapper to next both headings. Instead, it puts "slot='card-header'" on both heading elements. 
+            </p>
+            <p>
+              Notice, we are able to add styles because they <strong>ARE</strong> the slotted element.
+            </p>
+            <div slot="card-footer">
+              <a href="#" class="usa-button">
+                click me
+              </a>
+            </div>
+          </usa-card>
+          <usa-card headerFirst layout="flag">
+            <img slot="card-media" src="${media}" alt="Placeholder image">
+            <div slot="card-header">
+              <h2 class="test-card__heading" part="cardheading">${title || "Default flag"}</h2>
+            </div>
+            <p>
+              ${content}
+            </p>
+            <div slot="card-footer">
+              <a href="#" class="usa-button">${buttonText}</a>
+            </div>
+          </usa-card>
+          <usa-card>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam neque ullam quia harum quaerat debitis, et minima fugit expedita hic?
+            <button class="usa-button">
+              Click me
+            </button>
+          </usa-card>
+          <usa-card exdent>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam neque ullam quia harum quaerat debitis, et minima fugit expedita hic?
             <button class="usa-button">
               Click me
