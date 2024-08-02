8d4ed2055f4298868dd8ae57594154c35273aa1a
Matt Henry <matt.henry@gsa.gov>
2024-08-02 09:58:34 -0400

    Just export right from the index

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
 import { UsaLink } from "./usa-link";
 import { UsaBanner } from "./usa-banner";
-/* eslint-enable no-unused-vars */
+
+export { UsaLink, UsaBanner };
