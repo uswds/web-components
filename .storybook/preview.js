0fd8c4ac854f379da9d9018f1563052725f67400
James Mejia <james.mejia@gsa.gov>
2024-09-04 11:19:27 -0500

    Create global stylesheets for shared CSS vars

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,8 +2,11 @@ import UswdsTheme from "./UswdsTheme";
 
 // Global component styles.
 import "../storybook/index.css";
-// Theme overrides.
+// Storybook overrides.
 import "./index.css";
+
+// USWDS Core props.
+import "../src/core/index.css";
 
 /** @type { import('@storybook/web-components').Preview } */
 const preview = {
