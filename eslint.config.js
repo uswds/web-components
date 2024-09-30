36de343ff26be749e95feaaf6d8f171ef90b1dfb
James Mejia <james.mejia@gsa.gov>
2024-09-30 09:48:23 -0500

    Add eslint plugin for vitest
    Following plugin guidance[^1] to avoid ESLint errors in unit tests
    
    [^1]: https://github.com/vitest-dev/eslint-plugin-vitest

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,15 @@
 import js from "@eslint/js";
 import eslintConfigPrettierRecommended from "eslint-config-prettier";
+import vitest from "vitest";
 
-export default [js.configs.recommended, eslintConfigPrettierRecommended];
+export default [
+  js.configs.recommended,
+  eslintConfigPrettierRecommended,
+  {
+    files: ["**/*.spec.js"],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  },
+];
